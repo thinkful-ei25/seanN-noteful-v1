5ea1c2b2737a80146b9eccf5b94f9d26f3da4d66
Sean Nealon <signalflowsean@gmail.com>
2018-10-08 15:15:37 -0400

    get notes

--- server.js
+++ server.js
@@ -16,4 +16,10 @@ app.listen(8080, function() {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => { 
   console.log(err);
+});
+
+app.get('/api/notes', (req, res) => { 
+  res.json(data); 
 }); 
+
+
