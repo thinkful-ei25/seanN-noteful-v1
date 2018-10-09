a45b577f81db453b12852e952b660c1e43f493e3
Sean Nealon <signalflowsean@gmail.com>
2018-10-09 14:57:59 -0400

    error handling

--- server.js
+++ server.js
@@ -27,11 +27,26 @@ app.get('/api/notes/:id', (req, res) => {
 
 });
 
+app.get('/boom', (req, res, next) =>{ 
+  throw new Error('Boom!!'); 
+}); 
+
 app.use((req, res, next) => { 
   let err = new Error('Not Found'); 
   err.status = 404; 
-  res.status(404).json({message : 'No Unicorns Here!!!'}); 
+  res.status(404).json({message : 'No Unicorns Here!'}); 
 }); 
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+
+
 
 // Listen for incoming connections
 app.listen(PORT, function () {
