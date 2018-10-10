bfd6ca77b023c6279031342cf466cc8178d51eb5
Sean Nealon <signalflowsean@gmail.com>
2018-10-10 15:22:22 -0400

    move endpoints to exoress router

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 'use strict';
 
 const {PORT} = require('./config'); 
-// const {logger} = require('./middleware/logger'); 
 const morgan = require('morgan'); 
 const express = require('express');
 
 // Load array of notes
-const data = require('./db/notes');
-const simDB = require('./db/simDB'); 
-const notes = simDB.initialize(data); 
+const notesRouter = require('./router/notes.router.js'); 
 
 // Create an Express application
 const app = express();
+
+app.use('/api/notes', notesRouter); 
 
 // Log all requests
 app.use(morgan('common')); 
@@ -21,102 +20,6 @@ app.use(express.static('public'));
 
 // Parse request body
 app.use(express.json());
-
-
-// Get All (and search by query)
-app.get('/api/notes', (req, res) => {
-  const { searchTerm } = req.query;
-  //res.json(searchTerm ? data.filter(item => item.title.includes(searchTerm)) : data);
-  
-  notes.filter(searchTerm, (err, list, next) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
-});
-
-// Get a single item
-app.get('/api/notes/:id', (req, res) => {
-  //const id = req.params.id;
-  //res.json(data.find(item => item.id === Number(id)));
-  const { id } = req.params; 
-
-  notes.find(id, (err, item, next) => { 
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(item); 
-  });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-app.get('/boom', (req, res, next) =>{ 
-  throw new Error('Boom!!'); 
-}); 
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-app.use((req, res, next) => { 
-  let err = new Error('Not Found'); 
-  err.status = 404; 
-  res.status(404).json({message : 'No Unicorns Here!'}); 
-}); 
-
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
-});
 
 // Listen for incoming connections
 app.listen(PORT, function () {
